2db0a19c28f6c202308a866903e632413f7b8cab
l0n3r <mirshohid1214@gmail.com>
2024-11-29 16:20:16 +0500

    views have been created

--- books/views.py
+++ books/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer, OrderSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def book_list_create_api_view(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def order_create_api_view(request):
+    serializer = OrderSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def author_detail_api_view(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    serializer = AuthorSerializer(author)
+    return Response(serializer.data)
